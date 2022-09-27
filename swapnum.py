844965b4b9864765a7722a9d212d5de2e8d3acdc
ayan-adak <101331393+ayan-adak@users.noreply.github.com>
2022-09-27 12:16:41 +0530

    Create swapnum.py

---
+++ swapnum.py
@@ -0,0 +1,9 @@
+a = input('Enter First Number: ')
+b = input('Enter Second Number: ')
+
+temp = a
+a = b
+b = temp
+
+print('The Value of X After Swapping : {}' .format(a))
+print('The Value of Y After Swapping : {}' .format(b))
